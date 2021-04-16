c17e1305c50a36c972c1ec5ac5218f6c9e0b82ce
magda.krok <magdalena.krok@interia.pl>
2021-04-16 13:44:35 +0200

    context-final

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -1,30 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import DeleteButton from 'components/atoms/DeleteButton/DeleteButton';
 import { Wrapper, StyledAverage, StyledInfo } from './UserListItem.styles';
+import { UsersContext } from 'providers/UsersProvider';
+import { UserShape } from 'types';
 
-const UsersListItem = ({
-  deleteUser,
-  userData: { average, name, attendance = '0%' },
-}) => (
-  <Wrapper>
-    <StyledAverage avg={average}>{average}</StyledAverage>
-    <StyledInfo>
-      <p>
-        {name}
-        <DeleteButton onClick={() => deleteUser(name)} />
-      </p>
-      <p>attendance: {attendance}</p>
-    </StyledInfo>
-  </Wrapper>
-);
+const UsersListItem = ({ userData: { average, name, attendance = '0%' } }) => {
+  const { deleteUser } = useContext(UsersContext);
+
+  return (
+    <Wrapper>
+      <StyledAverage avg={average}>{average}</StyledAverage>
+      <StyledInfo>
+        <p>
+          {name}
+          <DeleteButton onClick={() => deleteUser(name)} />
+        </p>
+        <p>attendance: {attendance}</p>
+      </StyledInfo>
+    </Wrapper>
+  );
+};
 
 UsersListItem.propTypes = {
-  userData: PropTypes.shape({
-    average: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    attendance: PropTypes.string,
-  }),
+  userData: PropTypes.shape({ UserShape }),
 };
 
 export default UsersListItem;
